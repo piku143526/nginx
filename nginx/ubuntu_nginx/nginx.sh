7bf10c5d64c81f842875b23db599af3eca3fe204
Pradeep <104751531+piku143526@users.noreply.github.com>
2023-09-28 00:41:25 +0530

    Update nginx.sh

--- nginx/ubuntu_nginx/nginx.sh
+++ nginx/ubuntu_nginx/nginx.sh
@@ -8,8 +8,8 @@ sudo systemctl start nginx
 cd /etc/nginx/conf.d/
 sudo chmod 757 /etc/nginx/conf.d/
 cd /etc/nginx/conf.d/
-sudo wget  https://github.com/piku143526/nginx/raw/main/api.conf
-sudo wget  https://github.com/piku143526/nginx/raw/main/app.conf
+sudo wget  https://github.com/piku143526/nginx/raw/main/nginx/ubuntu_nginx/api.conf
+sudo wget  https://github.com/piku143526/nginx/raw/main/nginx/ubuntu_nginx/app.conf
 sudo chmod 755 /etc/nginx/conf.d/
 cd /var/www/vhosts/frontend/
 sudo wget https://github.com/piku143526/nginx/raw/main/index.html
