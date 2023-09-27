60d8d1cf560df035221c27b432473708c44ed173
Pradeep <104751531+piku143526@users.noreply.github.com>
2023-09-28 00:44:51 +0530

    Update nginx.sh

--- nginx/ubuntu_nginx/nginx.sh
+++ nginx/ubuntu_nginx/nginx.sh
@@ -4,7 +4,7 @@ sudo mkdir -p  /var/www/vhosts/frontend
 sudo chown -R $USER:$USER /var/www/vhosts/frontend/
 cd /var/www/vhosts/frontend/
 sudo apt install nginx -y
-sudo systemctl start nginx
+sudo systemctl start nginx.service
 cd /etc/nginx/conf.d/
 sudo chmod 757 /etc/nginx/conf.d/
 cd /etc/nginx/conf.d/
@@ -13,6 +13,6 @@ sudo wget  https://github.com/piku143526/nginx/raw/main/nginx/ubuntu_nginx/app.c
 sudo chmod 755 /etc/nginx/conf.d/
 cd /var/www/vhosts/frontend/
 sudo wget https://github.com/piku143526/nginx/raw/main/nginx/ubuntu_nginx/index.html
-sudo ssystemctl restart nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
+sudo ssystemctl restart nginx.service 
+sudo systemctl enable nginx.service 
+sudo systemctl status nginx.service 
